9000a1bf3a63f308e8f09225b5455e5e6d99a367
Pritam280 <pritampharande7@gmail.com>
2026-02-24 10:10:52 +0530

    stages added

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -6,5 +6,42 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/Pritam280/flight-reservation.git'
             }
         }
+        stage('Build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    mvn clean package
+                '''
+            }
+        }
+        stage('QA-test'){
+            steps{
+                withSonarQubeEnv(installationName: 'sonar',credentialsId: 'sonar-token') {
+                    sh '''
+                        cd FlightReservationApplication
+                        mvn sonar:sonar -Dsonar.projectKey=flight-reservation 
+
+                    '''
+                }
+            }
+        }
+        stage('Docker-build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    docker build -t flight-reservation:latest 
+                    docker push alexaa123/flight-reservation:latest
+                    docker rmi alexaa123/flight-reservation:latest
+                '''
+            }
+        }
+        stage('Deploy'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    kubectl apply -f k8s 
+                '''
+            }
+        }
     }
 }
