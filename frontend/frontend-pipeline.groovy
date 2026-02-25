7d9edffd86670c7d0aaa4ca398f286e56caf8d67
Pritam280 <pritampharande7@gmail.com>
2026-02-25 09:18:50 +0530

    3 changes

--- frontend/frontend-pipeline.groovy
+++ frontend/frontend-pipeline.groovy
@@ -6,22 +6,5 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/Pritam280/flight-reservation.git'
             }
         }
-        stage('Build'){
-            steps{
-                sh''' 
-                    cd frontend
-                    npm install
-                    npm run build
-                '''
-            }
-        }
-        stage('Deploy'){
-            steps{
-                sh '''
-                    cd frontend
-                    aws s3 sync dist/ s3://cblkdfsfdsc-front12end-project-bux/
-                '''    
-            }
-        }
     }
 }
